557acb30ba18ebb3b376cc5040ce9c21e1b03820
TomerNissim <tomernis@gmail.com>
2021-03-04 17:07:02 +0200

    error handling

--- app.js
+++ app.js
@@ -12,34 +12,49 @@ app.use(express.json());
 
 
 app.post("/api/shorturl/new", async (req, res) => {
-  let id = await DataBase.addUrl(req.body);
-  res.send("http://" + req.get("host") + "/" + id).status(200);
+  
+    let id = await DataBase.addUrl(req.body);
+    res.send("http://" + req.get("host") + "/" + id).status(200);
+  
 })
-
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/:id", async (req,res) => {
-  const id = req.params.id;
-  if(id === "favicon.ico"){
-    res.status(400);
-    return ;
-  }
-
-  let url =await DataBase.getOriginUrl(id);
-  if(url!== null){
-    res.redirect(url);
-  }
+    const id = req.params.id;
+    if(id === "favicon.ico"){
+      res.status(400);
+      return;
+    }
+    let url =await DataBase.getOriginUrl(id);
+    if(url.startsWith("htt")){
+      res.redirect(url);
+      return;
+    }
+    res.send(responseHandling(url));  
 })
+
 app.get("/api/statistic/:id", async (req,res) => {
   const id = req.params.id;
   let statistics = await DataBase.getStatistics(id);
-  if(statistics!== null){
-
-    res.send(statistics).status(200)
-  }
-  else res.send("hi") 
+    res.send(responseHandling(statistics))
 })
+
+function responseHandling(response){
+  switch(response){
+      case "400/short-url":
+          return {status: 400, error: "error" + ":" +" Invalid short-url format" };
+      case "400/protocol":
+          return {status: 400, error: "error" + ":" +" Invalid protocol" };    
+      case "404/statistic":
+          return {status: 404, error: "error" + ":" +" No statistic found for the given input" };
+      case "404/getOriginUrl":
+          return {status: 404, error: "error" + ":" +" No short URL found for the given input" }
+  }
+  return {status: 200, data: response}
+}
+
+
 module.exports = app;
--- database.js
+++ database.js
@@ -1,20 +1,23 @@
 const shortid = require("shortid");
 const fs = require("fs").promises;
+const dir = process.env.NODE_ENV === 'test' ? './test':'./data';
 
 class DataBase {
     static items = [];
 
     static async readData(){
-        const data = await fs.readFile("data.json", "utf8", function (err, data) {
+        const data = await fs.readFile(`${dir}.json`, "utf8", function (err, data) {
             if(err){
                 return console.log(err);
             }
-            console.log(data);
         });
         this.items = JSON.parse(data);
     }
 
     static async addUrl(body){
+        if(!isValidProtocol(body)){
+            return "400/protocol"
+        }
         await this.readData();
         for(let item of this.items){
             if(body.url === item.originalUrl)
@@ -27,26 +30,31 @@ class DataBase {
             id: shortid.generate()
         };
         this.items.push(item);
-        fs.writeFile("data.json", JSON.stringify(this.items), function (err){
+        fs.writeFile(`${dir}.json`, JSON.stringify(this.items), function (err){
             console.log("data-saved");
         })
         return item.id;
     }
 
-    static async getOriginUrl(id) {
+    static async getOriginUrl(id) { 
+        if(!shortid.isValid(id)){
+            return "400/short-url";
+        }
         await this.readData();
         for(let item of this.items){
             if(id === item.id){
                 item.redirectCount += 1;
-                fs.writeFile("data.json", JSON.stringify(this.items));
+                fs.writeFile(`${dir}.json`, JSON.stringify(this.items));
                 return item.originalUrl;
             }
         } 
-        return null;    
+        return "404/getOriginUrl";    
     }
 
     static async getStatistics(shortUrl){
-        // shortUrl = shortUrl.toString() ;
+        if(!shortid.isValid(shortUrl)){
+            return "400/short-url";
+        }
         await this.readData();
         console.log(shortUrl);
         for(let item of this.items){
@@ -58,10 +66,16 @@ class DataBase {
                 } 
             }
         }
-        return null
+        return "404/statistic"
     }
 
-   
+}
+
+function isValidProtocol(url){
+    if(url.startsWith("http://") || url.startsWith("https://")){
+        return true;
+    }
+    return false;
 }
 function convertDate(date){
     return date.toISOString().toString().replace("T"," ").substr(0,19);
